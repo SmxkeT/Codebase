a3c82831b395112482def1ee195a4843562766f2
SmxkeT <tddoyle159@outlook.com>
2023-12-19 16:44:32 +0000

    generic page to test Javascript | Scripts.js to run code on webpage.

---
+++ www/scripts.js
@@ -0,0 +1,20 @@
+ //let = variables with equations | alert = displaying the answers
+      let numone = -5 + 8 * 6;
+      alert(numone)
+
+      let numtwo = (55 + 9) % 9;
+      alert(numtwo)
+
+      let numthree = 20 + -3 * 5 / 8;
+      alert(numthree)
+
+      let numfour = 5 + 15 / 3 * 2 - 8 % 3
+      alert(numfour)
+
+      
+      //Two variables to ask for number inputs.
+      let usernum1 = prompt("Enter a number: ");
+      let usernum2 = prompt("Enter a second number: ");
+
+      //divides both user input numbers and displays the answer.
+      alert(usernum1 / usernum2)
