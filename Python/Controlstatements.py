1724510260c0eff1e81a5cc219aa56fcf6abee5c
SmxkeT <153958334+SmxkeT@users.noreply.github.com>
2024-03-01 10:40:07 +0000

    Add EX 2

--- Python/Controlstatements.py
+++ Python/Controlstatements.py
@@ -10,8 +10,14 @@ def math3(a, b, c, d):
 def math4(a, b, c, d, e, f):
     print(a+b / c*d -e %f)
 
+def usermath(): #Function that takes 2 numbers and divides and displays the result.
+    num1 = int(input("Enter a number; "))
+    num2 = int(input("Enter a second number; "))
+    print(f"Dividing the number has returned: {num1 / num2}!") 
 
 math1(5, 8, 6)  #calling the functions and filling them with numbers to be calculated with the above formulas.
 math2(55, 9, 9)
 math3(20, 3, 5, 8)
 math4(5, 15, 3, 2, 8, 3)
+
+usermath() #calling  function
