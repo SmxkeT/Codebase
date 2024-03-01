1022842f767aebbc0c976d2bd4bca2dd113aa1ab
SmxkeT <153958334+SmxkeT@users.noreply.github.com>
2024-03-01 10:36:06 +0000

    Add files via upload

---
+++ Python/Controlstatements.py
@@ -0,0 +1,17 @@
+def math1(a, b, c): #4 functions with math formulas ready for 4 arguments to calculate.
+    print(-a +b*c)
+
+def math2(a, b, c):
+    print((a+b)%c)
+
+def math3(a, b, c, d):
+    print(a + -b*c / d)
+
+def math4(a, b, c, d, e, f):
+    print(a+b / c*d -e %f)
+
+
+math1(5, 8, 6)  #calling the functions and filling them with numbers to be calculated with the above formulas.
+math2(55, 9, 9)
+math3(20, 3, 5, 8)
+math4(5, 15, 3, 2, 8, 3)
